297300dffd5d1298c121fea5852c634111cbeb7b
James Gallagher <jgallagher@opendap.org>
2022-01-11 18:46:04 -0700

    removed sqlite-libs since it's not present on c7.
    [skip ci]

--- build-for-rpm-part-4.sh
+++ build-for-rpm-part-4.sh
@@ -12,7 +12,8 @@
 # -u: Treat unset variables in substitutions as errors (except for @ and *)
 set -eu
 
-yum -y install sqlite-libs sqlite-devel
+yum -y install sqlite-devel
+# for Centos8, we may need 'sqlite-libs,' too.
 
 export CONFIGURE_FLAGS="--disable-shared"
 
