82f1707ed57a94706cd6a24d1d246d5098dba7fa
James Gallagher <jgallagher@opendap.org>
2023-01-27 16:36:39 -0700

    Remove CentOS 7 and clean up cruft

--- retired/build-for-rpm.sh
+++ retired/build-for-rpm.sh
@@ -5,21 +5,17 @@
 # (or the CentOS7 version).
 #
 # Modified to take an optional parameter that denotes the version of the C++
-# compiler to use. Since C6 lacks a C++-11 compiler, this can be used to supress
+# compiler to use. Since C6 lacks a C++-11 compiler, this can be used to suppress
 # building some of the dependencies. jhrg 10/28/19
 
 # -e: Exit immediately if a command, command in a pipeline, etc., fails
 # -u: Treat unset variables in substitutions as errors (except for @ and *)
 set -eu
 
-yum -y install sqlite-devel
-# for Centos8, we may need 'sqlite-libs,' too.
+printenv
 
-export CONFIGURE_FLAGS="--disable-shared"
+# Hack - update this container and remove this or switch to the vanilla
+# CentOS 7 container and use the Docker file. jhrg 3/3/21
+yum -y install libpng-devel
 
-cd /root/hyrax-dependencies
-
-make -j16 ci-part-1
-make -j16 ci-part-2
-make -j16 ci-part-3
-make -j16 ci-part-4
+(cd /root/hyrax-dependencies && make -j4 for-static-rpm)
