ae9bb88a254ac378b85122b01cf0b7f9f7085a2b
ndp-opendap <ndp@opendap.org>
2025-11-24 11:37:45 -0800

    wip

--- build-for-rocky8.sh
+++ build-for-rocky8.sh
@@ -15,11 +15,12 @@
 # -u: Treat unset variables in substitutions as errors (except for @ and *)
 set -eu
 
-dnf -y update
-
-# Build only the static libraries so that when these are used during the BES
-# RPM build we have packages that others can install. jhrg 2/8/22
-#
+HR="#########################################################################"
+###########################################################################
+# loggy()
+function loggy(){
+    echo  "$@" | awk '{ print "# rocky8 - "$0;}'  >&2
+}
 # This is not needed when the 'for-static-rpm' target is used below. That is
 # a more robust way to build the static packages since some of them might not
 # use configure, but cmake, e.g. jhrg 10/10/25
@@ -28,15 +29,39 @@ dnf -y update
 export CPPFLAGS=-I/usr/include/tirpc
 export LDFLAGS=-ltirpc
 
+
+loggy "$HR"
+loggy "BEGIN $0"
+loggy "Inside the docker container."
+loggy "  BUILD_NUMBER: $BUILD_NUMBER"
+loggy "        prefix: $prefix"
+loggy "          HOME: $HOME"
+loggy "          PATH: $PATH"
+loggy "       LDFLAGS: $LDFLAGS"
+loggy "      CPPFLAGS: $CPPFLAGS"
+loggy "redhat-release: \"$(cat /etc/redhat-release)\""
+loggy "   Environment:"
+loggy "$(env)"
+loggy "uname -a: $(uname -a)"
+loggy "- - - - - - - - - - - - - - - - - - - - -"
+loggy "Running dnf update"
+dnf -y update
+
+
 # Assume that the docker container has been started with the cloned repo
 # mounted so it appears within 'root.'
 cd /root/hyrax-dependencies
 
+loggy "Running: make for-static-rpm"
 make -j16 for-static-rpm
 
 make list-built
 
 # Now clean out the binary images, which are huge for a static build.
 # NB prefix = /root/install, set by the Docker run command
+loggy "Cleanup..."
 rm -f $prefix/deps/bin/{gdal_*,gdal[a-z]*,ogr*,gnm*,nearblack,testepsg}
 rm -rf $prefix/deps/proj-6/bin;
+
+loggy "END - $0"
+
