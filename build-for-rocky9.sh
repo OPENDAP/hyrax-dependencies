ed6a4e89de32be167f6e8997cbb3f87b96f1db26
ndp-opendap <ndp@opendap.org>
2025-11-24 10:19:29 -0800

    wip

--- build-for-rocky9.sh
+++ build-for-rocky9.sh
@@ -34,9 +34,13 @@ function loggy(){
     echo  "$@" | awk '{ print "# "$0;}'  >&2
 }
 
-export CPPFLAGS="-I/usr/include/tirpc"
+export CPPFLAGS="$CPPFLAGS"
+export CPPFLAGS="$CPPFLAGS -I/usr/include/tirpc"
+export LDFLAGS="${LDFLAGS:-""} -ltirpc"
 
-export LDFLAGS="-ltirpc"
+export CPPFLAGS="${CPPFLAGS:-""} -I/usr/include/curl"
+export LDFLAGS="${LDFLAGS:-""} -lcurl"
+
 
 loggy "$HR"
 loggy "BEGIN $0"
